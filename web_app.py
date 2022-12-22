e6978f3e71939c8e76c0bbef00976322f69c1a98
Erwin Michiels <erwin.michiels@student.odisee.be>
2022-12-22 13:37:56 +0000

    deployment

--- web_app.py
+++ web_app.py
@@ -10,4 +10,4 @@ def main():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    microweb_app.run(host="0.0.0.0", port=5050)
+    microweb_app.run(host="0.0.0.0", port=8899)
